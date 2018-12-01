836356d4c63a331d3b8ca00501a54a1cc5e452f7
Marcus Alfredsson <marcus@alfredo.se>
2018-12-01 13:21:13 +0100

    Fixed positioning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import * as style from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={style.App}>
+        <div className={style['App-header__title']}>
+          Todo app
+        </div>
+        <div className={style['App-header__symbol']}>
+          <img src={logo} className={style['App-logo']} alt="logo" />
+        </div>
+        <div className={style.content}>
+        test content
+        </div>
       </div>
     );
   }
